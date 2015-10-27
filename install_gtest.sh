70a55bcd2c46c7eb9cb5f7722b7e116096ff231e
jereksel <jereksel@gmail.com>
2015-10-27 22:40:37 +0100

    Downgrade cmake

---
+++ install_gtest.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -e
+wget https://googletest.googlecode.com/files/gtest-1.7.0.zip
+unzip gtest-1.7.0.zip
+mkdir gtest-1.7.0/build
+cd gtest-1.7.0/build
+cmake ..
+make
+export LD_LIBRARY_PATH = $LD_LIBRARY_PATH:../build
